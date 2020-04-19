6c831dc263f3ca83dd33b0f73b75890bc1fe1913
yuxuan.tay <wyextay@gmail.com>
2020-04-19 20:04:11 +0800

    delete route

--- src/app/api/models.py
+++ src/app/api/models.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, PositiveInt
 
 
 class NoteSchema(BaseModel):
-    title: str
-    description: str
+    title: str = Field(..., min_length=3, max_length=50)
+    description: str = Field(..., min_length=3, max_length=50)
 
 
 class NoteDB(NoteSchema):
-    id: int
+    id: PositiveInt
--- src/tests/test_notes.py
+++ src/tests/test_notes.py
@@ -24,6 +24,9 @@ def test_create_note_invalid_json(test_app):
     response = test_app.post("/notes/", data=json.dumps({"title": "something"}))
     assert response.status_code == 422
 
+    response = test_app.post("/notes/", data=json.dumps({"title": "1", "description": "2"}))
+    assert response.status_code == 422
+
 
 def test_read_note(test_app, monkeypatch):
     test_data = {"id": 1, "title": "something", "description": "something else"}
@@ -47,6 +50,9 @@ def test_read_note_incorrect_id(test_app, monkeypatch):
     response = test_app.get("/notes/999")
     assert response.status_code == 404
     assert response.json()["detail"] == "Note not found"
+
+    response = test_app.get("/notes/0")
+    assert response.status_code == 422
 
 
 def test_read_notes(test_app, monkeypatch):
@@ -86,6 +92,9 @@ def test_update_note(test_app, monkeypatch):
         [1, {}, 422],
         [1, {"description": "bar"}, 422],
         [999, {"title": "foo", "description": "bar"}, 404],
+        [1, {"title": "1", "description": "bar"}, 422],
+        [1, {"title": "foo", "description": "1"}, 422],
+        [0, {"title": "foo", "description": "bar"}, 422],
     ],
 )
 def test_update_note_invalid(test_app, monkeypatch, id, payload, status_code):
@@ -94,5 +103,37 @@ def test_update_note_invalid(test_app, monkeypatch, id, payload, status_code):
 
     monkeypatch.setattr(crud, "get", mock_get)
 
-    response = test_app.put(f"/notes/{id}/", data=json.dumps(payload), )
+    response = test_app.put(f"/notes/{id}/", data=json.dumps(payload),)
     assert response.status_code == status_code
+
+
+def test_remove_note(test_app, monkeypatch):
+    test_data = {"title": "something", "description": "something else", "id": 1}
+
+    async def mock_get(id):
+        return test_data
+
+    monkeypatch.setattr(crud, "get", mock_get)
+
+    async def mock_delete(id):
+        return id
+
+    monkeypatch.setattr(crud, "delete", mock_delete)
+
+    response = test_app.delete("/notes/1/")
+    assert response.status_code == 200
+    assert response.json() == test_data
+
+
+def test_remove_note_incorrect_id(test_app, monkeypatch):
+    async def mock_get(id):
+        return None
+
+    monkeypatch.setattr(crud, "get", mock_get)
+
+    response = test_app.delete("/notes/999/")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Note not found"
+
+    response = test_app.delete("/notes/0/")
+    assert response.status_code == 422
